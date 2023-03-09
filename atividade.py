e2d7602d295b99a1eb1660f6e6b8468f12f5de7d
DESKTOP-BV2KHFU\ACADEMICO <diego@latecnolog1a.com.br>
2023-03-08 21:57:08 -0400

    Alteracao

--- atividade.py
+++ atividade.py
@@ -1,1 +1,6 @@
+Nome[] = str(input("Digite o nome do Funcionário: "))
+Horas[] = int(input("Digite a quantidade de horas trabalhadas por esse funcionário: "))
+Turno[] = str(input("Digite o turno deste funcionário, sendo M para matutino V para vespertino e N para noturno:"))
+categoria[] = str(input("Digite a categoria do funcionario sendo G para gerente e O para operario: "))
+
 
